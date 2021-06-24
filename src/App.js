df74b1ec96f70c93240380a205f6431fd84571a6
kingjaejun <saqxtmxhf888@naver.com>
2021-06-24 15:21:06 +0900

    covid-19 kr

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Header from './components/Header';
+import Contents from './components/Contents';
 import './App.css';
-
-function App() {
+function App(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Contents />
     </div>
-  );
+  )
 }
 
 export default App;
